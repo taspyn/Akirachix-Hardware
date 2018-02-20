70d0483ca2d48914f4142f723ae10ad173b76e06
Student <Student>
2018-02-20 12:31:19 +0300

    flow chart in c++

---
+++ Firmware/cpp/flowchart.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int a;
+   int b;
+   cout<< "enter value of a";
+   cin>>a;
+   
+   cout<<"enter value of b";
+   cin>>b;
+  
+   if (a>b){
+       cout<<"a is greater ";
+       
+   }
+     else if(a==b){
+         cout<<"a equals b";
+         
+     }
+  
+     else {
+       cout<<"b is greater";  
+       }
+    
+    
+    
+    
+    
+
+    return 0;
+}
+
+
+
